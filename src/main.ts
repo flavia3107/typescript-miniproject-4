dbcbb782d467e154c6c5f26920a073bee0291e9a
Flavia Kosti <flaviakosti@gmail.com>
2025-09-12 13:33:37 -0400

    MiniProject-4: Add logo

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ import { setupCounter } from './counter.ts'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      <img src="${viteLogo}" class="logo_img" alt="Vite logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+      <img src="${typescriptLogo}" class="logo_img vanilla" alt="TypeScript logo" />
     </a>
     <h1>Vite + TypeScript</h1>
     <div class="card">
